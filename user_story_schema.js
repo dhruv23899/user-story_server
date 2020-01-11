1a267076350f97b44e04f50ce65f6df788f88f11
dhruv23899 <33550365+dhruv23899@users.noreply.github.com>
2020-01-11 17:59:06 +0530

    Added Documentation

--- user_story_schema.js
+++ user_story_schema.js
@@ -1,19 +1,21 @@
+//Mongoose package used to create Schemas for MongoDB.
 var mongoose = require("mongoose");
-
 var Schema = mongoose.Schema;
 
+// Creates Schema with required attributes and data types
 var User_Story_Schema = new Schema({
   title: { type: String, required: true, unique: true },
   desc: { type: String, required: true },
   priority: { type: String, required: true, unique: false },
   status: { type: String, required: true, unique: false },
-  //owner: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
   owner: { type:String,required:true},
-  link_to_ms:[{ type: String }],
+  link_to_ms:[{ type: String }],  //This attribute is an array which stores objects of type String.
   link_to_mf: [{ type: String }],
   progress: { type: Number, required: false, unique: false }
 });
 
+//User_Story object  will be used for exporting User_Story_Schema
 const User_Story = mongoose.model("User_Story_Schema", User_Story_Schema);
-
+// Below code used for exporting the Schemas
+// Used when we need to import the below Schemas in another file.
 module.exports = { User_Story: User_Story };
